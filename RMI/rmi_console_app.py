2f34758c0177cd9dda0c85e686fdfe1519ef92e7
Jason Heflinger <heflinjn@rose-hulman.edu>
2022-07-29 11:41:58 -0400

    added avtivity log

---
+++ RMI/rmi_console_app.py
@@ -0,0 +1,6 @@
+from rmi import sysPrint, usrPrint, handleInput
+appRunning = True
+sysPrint("Welcome to RMI (Console app)! Enter a command to get started!")
+while appRunning:
+    usrPrint("")
+    appRunning = handleInput(input())
